c688bd69765521fa5138fe42960442dc5844eecc
Mian Saif <miansaif360@gmail.com>
2022-12-12 01:19:26 +0500

    final

---
+++ database/migrations/2022_12_11_201451_patient_appointment.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('test');
+            $table->string('status');
+            $table->string('price');
+            $table->string('address');
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
